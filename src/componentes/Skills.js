eebd45f2c49c79ba8d03d9614d7b066ca80b0f4b
ylanna <yladacs@gmail.com>
2022-09-06 17:52:15 -0300

    page skills 2page

--- src/componentes/Skills.js
+++ src/componentes/Skills.js
@@ -1,5 +1,12 @@
+import { Container, Col, Row } from "react-bootstrap";
+
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+
 
   export const Skills = () => {
     const responsive = {
@@ -21,6 +28,40 @@ import "react-multi-carousel/lib/styles.css";
     }
   }; 
 
-  
+  return (
+    <section className="skill" id="skills">
+        <Container>
+            <Row> 
+                <Col>
+                <div className="skill-bx">
+                    <h2>
+                        Skills
+                    </h2>
+                    <p>Vamos falar das skills que tenho para oferecer! </p>
+                    <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                        <div className="item">
+                            <img src={meter1} alt="Image" />
+                            <h5> Web Development</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter2} alt="Image" />
+                            <h5> Mobile Development</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter3} alt="Image" />
+                            <h5> Software Engineer</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter1} alt="Image" />
+                            <h5> DevOps</h5>
+                        </div>
+                    </Carousel>
+                </div>
+                </Col>
+            </Row>
+        </Container>
+        <img className="background-image-left" src={colorSharp} />
+    </section>
+  )
 
 }
